bdaf43532269b95cd707a940ebdd7ceee6bdea03
Gustavo <gustavo_97@protonmail.com>
2024-04-22 20:34:09 -0300

    feat: slug value object

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,22 +1,25 @@
 import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
     title: string;
     content: string;
     authorId: string;
+    slug: Slug;
 }
 
 export class Question {
-    
     public title: string;
     public content: string;
     public authorId: string;
+    public slug: Slug;
     public id?: string;
 
     constructor(questionProps: QuestionProps, id?: string) {
         this.title = questionProps.title;
         this.content = questionProps.content;
         this.authorId = questionProps.authorId;
+        this.slug = questionProps.slug;
         this.id = id ?? randomUUID();
     }
 }
