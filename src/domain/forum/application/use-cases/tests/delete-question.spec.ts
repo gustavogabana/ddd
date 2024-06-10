bfc053224bc71d46fa8174847c92954d8c40c8c7
gustavogabana <gustavo_97@protonmail.com>
2024-06-10 20:55:07 -0300

    feat: verification of author id so that author A cannot delete question from author B

--- src/domain/forum/application/use-cases/tests/delete-question.spec.ts
+++ src/domain/forum/application/use-cases/tests/delete-question.spec.ts
@@ -1,8 +1,8 @@
 import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 import { makeQuestion } from "test/factories/make-question";
 import { DeleteQuestionUseCase } from "../delete-question";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { rejects } from "assert";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let sut: DeleteQuestionUseCase;
@@ -14,12 +14,27 @@ describe('Delete Question', () => {
     });
 
     it('should be able to delete a question', async () => {
-        const newQuestion = makeQuestion({}, new UniqueEntityId('question-1'));
+        const newQuestion = makeQuestion({
+            authorId: new UniqueEntityId('author-1')
+        }, new UniqueEntityId('question-1'));
 
         await inMemoryQuestionsRepository.create(newQuestion);
 
-        await sut.execute({ questionId: newQuestion.id.toString() });
+        await sut.execute({ questionId: newQuestion.id.toString(), authorId: newQuestion.authorId.toString() });
     
         expect(inMemoryQuestionsRepository.items).toHaveLength(0);
+    });
+
+    it('it should not be able to delete a question from another user', async () => {
+        const newQuestion = makeQuestion({
+            authorId: new UniqueEntityId('author-1')
+        }, new UniqueEntityId('question-1'));
+
+        await inMemoryQuestionsRepository.create(newQuestion);
+
+        expect(async () => {
+            return await sut.execute({ questionId: newQuestion.id.toString(), authorId: 'question-2' })
+        }).rejects.toBeInstanceOf(Error);
+
     });
 });
